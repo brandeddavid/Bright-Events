47d2ce182d7e6d6b81a5a796ae14e45d8220e1f3
DavidMwangi <david.mathenge98@gmail.com>
2017-11-22 14:01:02 +0300

    Add user user exists test

--- tests/user_tests.py
+++ tests/user_tests.py
@@ -16,3 +16,11 @@ class Testcreateuser(unittest.TestCase):
         result = self.user.createUser('dmwangi','dmwangi@gmail.com','dmwangi','dmwangi')
 
         self.assertEqual(result, 'Registration Successful')
+
+    def test_user_user_already_exists(self):
+
+        self.user.createUser('dmwangi','dmwangi@gmail.com','dmwangi','dmwangi')
+
+        result = self.user.createUser('dmwangi','dmwangi@gmail.com','dmwangi','dmwangi')
+
+        self.assertEqual(result, 'Email already has an account')
