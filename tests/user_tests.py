3a13a7110f98be38ac4401caff56ab38cd889247
DavidMwangi <david.mathenge98@gmail.com>
2017-11-22 14:57:30 +0300

    Doc Strings

--- tests/user_tests.py
+++ tests/user_tests.py
@@ -4,20 +4,26 @@ from app import models
 
 class Testcreateuser(unittest.TestCase):
 
-    """ Class for testing user creation"""
+    """ Class for testing user creation model"""
 
     def setUp(self):
+
+        """Instance of class user"""
 
         self.user = models.User()
 
 
     def test_user_registration_success(self):
 
+        """Test for successful user registration"""
+
         result = self.user.createUser('dmwangi','dmwangi@gmail.com','dmwangi','dmwangi')
 
         self.assertEqual(result, 'Registration Successful')
 
     def test_user_already_exists(self):
+
+        """Tests for already existing users"""
 
         self.user.createUser('dmwangi','dmwangi@gmail.com','dmwangi','dmwangi')
 
@@ -27,17 +33,25 @@ class Testcreateuser(unittest.TestCase):
 
     def test_user_password_mismatch(self):
 
+        """Tests for missmatched password"""
+
         result = self.user.createUser('dmwangi','dmwangi@gmail.com','dmwangi','notme')
 
         self.assertEqual(result, 'Passwords do not match')
 
 class Testloginuser(unittest.TestCase):
 
+    """Tests user login method"""
+
     def setUp(self):
+
+        """Instance of user class"""
 
         self.user = models.User()
 
     def test_user_login_success(self):
+
+        """Tests Successful  user login"""
 
         self.user.createUser('dmwangi','dmwangi@gmail.com','dmwangi','dmwangi')
 
@@ -47,6 +61,8 @@ class Testloginuser(unittest.TestCase):
 
     def test_user_password_mismatch(self):
 
+        """Tests password mismatch during login"""
+
         self.user.createUser('dmwangi','dmwangi@gmail.com','dmwangi','dmwangi')
 
         result = self.user.login('dmwangi@gmail.com','wrongpas')
@@ -54,6 +70,8 @@ class Testloginuser(unittest.TestCase):
         self.assertEqual(result, 'Password do not match')
 
     def test_user_not_signedup(self):
+
+        """Tests for user not signed up"""
 
         self.user.createUser('dmwangi','dmwangi@gmail.com','dmwangi','dmwangi')
 
