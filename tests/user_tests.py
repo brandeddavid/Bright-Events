59dd3ba539b5ff7efc8d07b6c4a8194c5eb3bd05
DavidMwangi <david.mathenge98@gmail.com>
2017-11-22 14:12:30 +0300

    Add password mismatch test

--- tests/user_tests.py
+++ tests/user_tests.py
@@ -17,10 +17,22 @@ class Testcreateuser(unittest.TestCase):
 
         self.assertEqual(result, 'Registration Successful')
 
-    def test_user_user_already_exists(self):
+    def test_user_already_exists(self):
 
         self.user.createUser('dmwangi','dmwangi@gmail.com','dmwangi','dmwangi')
 
         result = self.user.createUser('dmwangi','dmwangi@gmail.com','dmwangi','dmwangi')
 
         self.assertEqual(result, 'Email already has an account')
+
+    def test_user_password_mismatch(self):
+
+        result = self.user.createUser('dmwangi','dmwangi@gmail.com','dmwangi','notme')
+
+        self.assertEqual(result, 'Passwords do not match')
+
+class Testloginuser(unittest.TestCase):
+
+    def setUp(self):
+
+        self.user = models.User()
